7f276acd07bce2a8eae3f9132f8d1a4521438eda
Chris Mann <cmann@atlassian.com>
2016-03-11 16:44:45 +1100

    Take directory as argument.

--- src/ctdp/config.clj
+++ src/ctdp/config.clj
@@ -4,6 +4,5 @@
   {:erroneous-infrared-threshold 0.2
    :infrared-iso-value-threshold 999
    :language :en
-   :rootdir "/home/chris/testdata"
    :sunlight-end-hour 17
    :sunlight-start-hour 9})
--- src/ctdp/core.clj
+++ src/ctdp/core.clj
@@ -5,9 +5,11 @@
             [cats.monad.either :as either]
             [taoensso.tower :as tower]))
 
-(let [state {:config config
-             :translations (tower/make-t tconfig)}
-      warn ((:translations state) (:language (:config state)) :error/warn)
-      errfn #(printf "[%s] %s: %s\n" warn %1 %2)]
-  (map (fn [[k v]] (either/branch v (partial errfn k) clojure.pprint/pprint))
-       (pm/data-from-tree state (clojure.java.io/file (:rootdir config)))))
+(defn run
+  [dir]
+  (let [state {:config config
+               :translations (tower/make-t tconfig)}
+        warn ((:translations state) (:language (:config state)) :error/warn)
+        errfn #(printf "[%s] %s: %s\n" warn %1 %2)]
+    (map (fn [[k v]] (either/branch v (partial errfn k) clojure.pprint/pprint))
+         (pm/data-from-tree state (clojure.java.io/file dir)))))
