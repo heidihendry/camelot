5ca157a6ad823480b2a6e99dea2ab58bdb7aeec2
Chris Mann <chris@bitpattern.com.au>
2016-04-10 18:32:52 +1000

    Fix release script.

--- project.clj
+++ project.clj
@@ -31,7 +31,7 @@
   :plugins [[lein-cljsbuild "1.1.1"]
             [lein-environ "1.0.1"]]
 
-  :min-lein-version "0.2.2-SNAPSHOT"
+  :min-lein-version "2.6.1"
 
   :source-paths ["src/clj" "src/cljs" "dev"]
 
