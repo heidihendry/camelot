0db1b0d71e2cd6aa86aa7050d753ea5f24f8609e
Chris Mann <chris@bitpattern.com.au>
2016-04-21 22:36:11 +1000

    Fix release script.

--- script/release.sh
+++ script/release.sh
@@ -36,22 +36,23 @@ echo "Running camelot and ensuring it responds... "
 java -jar target/camelot.jar &
 PID=$!
 sleep 10
-set +e
 # Ensure process is still running
-ps -p $PID || exit 1
+ps -p $PID
 # Check the compiled JS is served
-curl -s http://localhost:8080/js/compiled/camelot.js > /dev/null || exit 1
+curl -s http://localhost:8080/js/compiled/camelot.js > /dev/null
 # Stop process
-curl -s -X POST http://localhost:8080/quit || exit 0
-sleep 1;
+curl -s -X POST http://localhost:8080/quit || true
 # Ensure process is stopped
+set +e
 ps -p $PID > /dev/null
 if [ $? -eq 0 ]; then
+    set -e
     echo "camelot seems to not be listening to requests. aborting."
     kill $PID
-    exit 1
+    false
+else
+    set -e
 fi
-set -e
 
 echo "Bumping release version... "
 sed -i "s/${PROJECT_NAME}\s\"\([0-9]\+\.[0-9]\+\.[0-9]\+\)-SNAPSHOT\"$/${PROJECT_NAME} \"\1\"/" ${PROJECT_FILE}
