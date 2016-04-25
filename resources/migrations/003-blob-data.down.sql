ea71d8d2ad80b52fd8679197945f5f061fee9e6d
Chris Mann <chris@bitpattern.com.au>
2016-04-25 21:41:20 +1000

    Add migrations for species and trap location blobs.
    Closes: #50
    Closes: #35

---
+++ resources/migrations/003-blob-data.down.sql
@@ -0,0 +1,3 @@
+ALTER TABLE species DROP COLUMN species_reference_image
+--;;
+ALTER TABLE trap_station DROP COLUMN trap_station_reference_image
---
+++ resources/migrations/003-blob-data.up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE species ADD COLUMN species_reference_image BLOB
+--;;
+ALTER TABLE trap_station ADD COLUMN trap_station_reference_image BLOB
