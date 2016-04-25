d556de6c7b584767b6aaee815988758add8f3b65
Chris Mann <chris@bitpattern.com.au>
2016-04-25 20:49:27 +1000

    Use Bitbucket for snapshot uploads.

--- script/upload-snapshot.sh
+++ script/upload-snapshot.sh
@@ -2,6 +2,9 @@
 
 PROJECT_NAME="camelot"
 PROJECT_FILE="project.clj"
+DOWNLOADS_URL="https://api.bitbucket.org/2.0/repositories/cshclm/camelot/downloads/"
 
 snapshot_version="$(grep -oE "[0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT" ${PROJECT_FILE} | head -n1)"
-gdrive upload "target/${PROJECT_NAME}.jar" --name "$PROJECT_NAME-${snapshot_version}-$(date '+%Y%m%d%H%M%S').jar" -p ${CAMELOT_GDRIVE_SNAPSHOT_PARENT}
+cp "target/${PROJECT_NAME}.jar" "target/$PROJECT_NAME-${snapshot_version}.jar"
+curl -u ${BITBUCKET_CREDENTIALS} -X POST ${DOWNLOADS_URL} -F "files=@target/$PROJECT_NAME-${snapshot_version}.jar"
+rm "target/$PROJECT_NAME-${snapshot_version}.jar"
