527a3f8e7d92d80f932d84bc74b488c52aa9827a
Chris Mann <chris@bitpattern.com.au>
2016-04-28 19:58:03 +1000

    Permit NULL GPS
    Closes: #58

---
+++ resources/migrations/004-permit-null-gps-data.down.sql
@@ -0,0 +1,3 @@
+ALTER TABLE trap_station ALTER COLUMN trap_station_longitude NOT NULL
+--;;
+ALTER TABLE trap_station ALTER COLUMN trap_station_latitude NOT NULL
---
+++ resources/migrations/004-permit-null-gps-data.up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE trap_station ALTER COLUMN trap_station_longitude NULL
+--;;
+ALTER TABLE trap_station ALTER COLUMN trap_station_latitude NULL
