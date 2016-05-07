ffcbef1ab05b6c9061ef93dacb3d6e943e2f5756
Chris Mann <chris@bitpattern.com.au>
2016-05-07 21:33:20 +1000

    Add unit tests for db.clj.

--- src/clj/camelot/db.clj
+++ src/clj/camelot/db.clj
@@ -15,6 +15,8 @@
    :migrations (jdbc/load-resources "migrations")})
 
 (defn- clj-key
+  "Reducer for translating from database types.
+  Translates table column names and SQL data types."
   [acc k v]
   (assoc acc (keyword (str/replace (name k) #"_" "-"))
          (if (instance? java.sql.Timestamp v)
@@ -22,32 +24,44 @@
            v)))
 
 (defn clj-keys
+  "Translate from database types for any type of collection.
+  Returns the given argument if it is not a collection."
   [data]
-  (when-not (nil? data)
-    (if (coll? data)
-      (if (seq? data)
-        (map #(into {} (reduce-kv clj-key {} %)) data)
-        (into {} (reduce-kv clj-key {} data)))
-      data)))
+  (cond
+      (seq? data) (map #(into {} (reduce-kv clj-key {} %)) data)
+      (vector? data) (mapv #(into {} (reduce-kv clj-key {} %)) data)
+      (map? data) (into {} (reduce-kv clj-key {} data))
+      :else data))
 
 (defn- db-key
+  "Reducer for transforming data into database-suitable types."
   [acc k v]
   (assoc acc (keyword (str/replace (name k) #"-" "_"))
          (if (instance? org.joda.time.DateTime v)
            (tc/to-sql-time v)
            v)))
 
+(defn db-keys
+  [data]
+  (reduce-kv db-key {} data))
+
 (defn with-db-keys
+  "Run a query, translating the parameters and results as needed."
   [f data]
-  (clj-keys (f (reduce-kv db-key {} data))))
+  (->> data
+       (db-keys)
+       (f)
+       (clj-keys)))
 
 (defn migrate
+  "Apply the available database migrations."
   []
   (rtc/migrate-all (:datastore config)
                    (rtc/into-index (:migrations config))
                    (:migrations config)))
 
 (defn rollback
+  "Rollback the last migration."
   []
   (rtc/rollback-last (:datastore config)
                      (rtc/into-index (:migrations config))))
---
+++ test/clj/camelot/db_test.clj
@@ -0,0 +1,57 @@
+(ns camelot.db-test
+  (:require [camelot.db :as sut]
+            [midje.sweet :refer :all]
+            [clj-time.core :as t]
+            [clj-time.coerce :as tc]))
+
+(facts "Converting from database types"
+  (fact "Converts columns in a single record"
+    (let [data {:column_name "MyData"}]
+      (sut/clj-keys data) => {:column-name "MyData"}))
+
+  (fact "Converts columns in a list of records"
+    (let [data '({:column_name "MyData"}
+                 {:column_name "Other Data"})]
+      (sut/clj-keys data) => '({:column-name "MyData"}
+                               {:column-name "Other Data"})))
+
+  (fact "Converts columns in a vector of records"
+    (let [data [{:column_name "MyData"}
+                {:column_name "Other Data"}]]
+      (sut/clj-keys data) => '({:column-name "MyData"}
+                               {:column-name "Other Data"})))
+
+  (fact "Returns non-collection data unchanged"
+    (let [data "The Number 5"]
+      (sut/clj-keys data) => data))
+
+  (fact "Returns nil when passed nil"
+    (let [data nil]
+      (sut/clj-keys data) => nil))
+
+  (fact "Converts from SQL Timestamps"
+    (let [date (t/date-time 2015 10 10 1 1 5)
+          date-long (tc/to-long date)
+          data {:column_name (java.sql.Timestamp. date-long)}]
+      (sut/clj-keys data) => {:column-name date})))
+
+(facts "Converting to database types"
+  (fact "Converts columns in a single record"
+    (let [data {:column-name "MyData"}]
+      (sut/db-keys data) => {:column_name "MyData"}))
+
+  (fact "Returns an empty hash when passed nil"
+    (let [data nil]
+      (sut/db-keys data) => {}))
+
+  (fact "Converts to SQL Timestamps"
+    (let [date (t/date-time 2015 10 10 1 1 5)
+          date-long (tc/to-long date)
+          data {:column-name date}]
+      (sut/db-keys data) => {:column_name (java.sql.Timestamp. date-long)})))
+
+(facts "Converting between database types"
+  (fact "Calls a function with the `db' data, returning a `clj' result."
+    (let [fn #(update % :column_name inc)
+          data {:column-name 5}]
+      (sut/with-db-keys fn data) => {:column-name 6})))
