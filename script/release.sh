06862b6c8b3320060353c99595eaf2cc4bcd0b53
Chris Mann <chris@bitpattern.com.au>
2016-08-07 17:07:23 +1000

    No longer test in release.

--- script/release.sh
+++ script/release.sh
@@ -6,57 +6,6 @@ PROJECT_NAME="camelot"
 PROJECT_FILE="project.clj"
 README_FILE="README.md"
 
-echo "Checking binaries in \$PATH... "
-which gdrive &> /dev/null
-which lein &> /dev/null
-which git &> /dev/null
-which sed &> /dev/null
-
-echo "Checking working directory is clean... "
-git diff --exit-code
-git diff --cached --exit-code
-
-echo "Cleaning... "
-lein clean
-
-echo "Checking code... "
-lein check
-
-echo "Compiling... "
-#lein with-profiles -dev,+production compile
-#lein with-profiles -dev,+uberjar cljsbuild once
-# Using uberjar over compile due to bug in Lein 2.6.1
-# https://github.com/technomancy/leiningen/issues/2096
-lein with-profiles -dev,-user,+uberjar uberjar
-
-echo "Running tests... "
-lein with-profiles +test test
-lein with-profiles +test midje
-lein doo phantom test once
-
-echo "Running camelot and ensuring it responds... "
-java -jar target/camelot.jar &
-PID=$!
-sleep 20
-# Ensure process is still running
-ps -p $PID
-# Check the compiled JS is served
-curl -s http://localhost:8080/js/compiled/camelot.js > /dev/null
-# Stop process
-curl -s -X POST http://localhost:8080/quit || true
-# Ensure process is stopped
-set +e
-ps -p $PID > /dev/null
-if [ $? -eq 0 ]; then
-    set -e
-    echo "camelot seems to not be listening to requests. aborting."
-    kill $PID
-    false
-else
-    echo "camelot is responsive."
-    set -e
-fi
-
 echo "Bumping release version... "
 sed -i "s/${PROJECT_NAME}\s\"\([0-9]\+\.[0-9]\+\.[0-9]\+\)-SNAPSHOT\"$/${PROJECT_NAME} \"\1\"/" ${PROJECT_FILE}
 released_version="$(grep -oE [0-9]+\.[0-9]+\.[0-9]+ ${PROJECT_FILE} | head -n1)"
