f700661d0ef71fcce65efd5a759df25229fc2de7
Chris Mann <chris@bitpattern.com.au>
2016-08-12 19:27:40 +1000

    Fix effort summary tests.

--- test/clj/camelot/report/module/builtin/reports/effort_summary_test.clj
+++ test/clj/camelot/report/module/builtin/reports/effort_summary_test.clj
@@ -18,6 +18,7 @@
   (sut/csv-report :effort-summary state {:survey-id id} data))
 
 (def headings ["Site ID"
+               "Site Name"
                "Site Area"
                "Number of Trap Stations"
                "Nights Elapsed"
@@ -27,6 +28,7 @@
 
 (def default-record
   {:site-id 1
+   :site-name "ASite"
    :site-area 1
    :trap-station-id 1
    :trap-station-session-start-date (t/date-time 2015 1 1 10 10 10)
@@ -55,7 +57,7 @@
                    (->record {}))
           state (gen-state-helper {})]
       (report state 1 records) =>
-      [[1 1 1 9 0 "2015/01" "2015/01"]]))
+      [[1 "ASite" 1 1 9 0 "2015/01" "2015/01"]]))
 
   (fact "Does not return a result should the survey not match"
     (let [records (list
@@ -68,31 +70,32 @@
                    (->record {:taxonomy-id 1})
                    (->record {:taxonomy-id 1}))
           state (gen-state-helper {})]
-      (report state 1 records) => [[1 1 1 9 1 "2015/01" "2015/01"]]))
+      (report state 1 records) => [[1 "ASite" 1 1 9 1 "2015/01" "2015/01"]]))
 
   (fact "Should return 2 species, should there be 2 distinct species"
     (let [records (list
                    (->record {:taxonomy-id 1})
                    (->record {:taxonomy-id 2}))
           state (gen-state-helper {})]
-      (report state 1 records) => [[1 1 1 9 2 "2015/01" "2015/01"]]))
+      (report state 1 records) => [[1 "ASite" 1 1 9 2 "2015/01" "2015/01"]]))
 
   (fact "Should return 2 trap stations, should there be 2 distinct trap stations"
     (let [records (list
                    (->record {:trap-station-id 1})
                    (->record {:trap-station-id 2}))
           state (gen-state-helper {})]
-      (report state 1 records) => [[1 1 2 9 0 "2015/01" "2015/01"]]))
+      (report state 1 records) => [[1 "ASite" 1 2 9 0 "2015/01" "2015/01"]]))
 
   (fact "Should return details for each site."
     (let [records (list
                    (->record {:site-id 1
                               :site-area 10})
                    (->record {:site-id 2
+                              :site-name "Another site"
                               :site-area 50}))
           state (gen-state-helper {})]
-      (report state 1 records) => [[1 10 1 9 0 "2015/01" "2015/01"]
-                                   [2 50 1 9 0 "2015/01" "2015/01"]]))
+      (report state 1 records) => [[1 "ASite" 10 1 9 0 "2015/01" "2015/01"]
+                                   [2 "Another site" 50 1 9 0 "2015/01" "2015/01"]]))
 
 (fact "Should return earliest session month for a site as the start period."
     (let [records (list
@@ -107,8 +110,8 @@
                               :trap-station-session-id 3
                               :trap-station-session-start-date (t/date-time 2014 7 15 1 1 1)}))
           state (gen-state-helper {})]
-      (report state 1 records) => [[1 1 1 9 0 "2015/01" "2015/01"]
-                                   [2 1 1 428 0 "2014/05" "2015/01"]]))
+      (report state 1 records) => [[1 "ASite" 1 1 9 0 "2015/01" "2015/01"]
+                                   [2 "ASite" 1 1 428 0 "2014/05" "2015/01"]]))
 
   (fact "Should return lastest session month for a site as the end period."
     (let [records (list
@@ -123,5 +126,5 @@
                               :trap-station-session-id 3
                               :trap-station-session-end-date (t/date-time 2016 2 28 1 1 1)}))
           state (gen-state-helper {})]
-      (report state 1 records) => [[1 1 1 9 0 "2015/01" "2015/01"]
-                                   [2 1 1 947 0 "2015/01" "2016/05"]])))
+      (report state 1 records) => [[1 "ASite" 1 1 9 0 "2015/01" "2015/01"]
+                                   [2 "ASite" 1 1 947 0 "2015/01" "2016/05"]])))
