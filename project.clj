695dd91e98a8cf43bef86eb2aa748e32c6b285d6
Chris Mann <chris@bitpattern.com.au>
2016-08-31 21:26:59 +1000

    Version bump: 0.5.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject camelot "0.5.6"
+(defproject camelot "0.5.7-SNAPSHOT"
   :description "Camelot - Camera Trap Management"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
