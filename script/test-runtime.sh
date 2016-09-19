8ecdab6cd878e0571471bc22ab1df57724e2e873
Chris Mann <chris@bitpattern.com.au>
2016-09-19 17:23:19 +1000

    Update port number used in instance sanity check.

--- script/test-runtime.sh
+++ script/test-runtime.sh
@@ -16,9 +16,9 @@ sleep 45
 # Ensure process is still running
 ps -p $PID
 # Check the compiled JS is served
-curl -s http://localhost:8080/js/compiled/camelot.js > /dev/null
+curl -s http://localhost:5341/js/compiled/camelot.js > /dev/null
 # Stop process
-curl -s -X POST http://localhost:8080/quit || true
+curl -s -X POST http://localhost:5341/quit || true
 # Ensure process is stopped
 set +e
 ps -p $PID > /dev/null
