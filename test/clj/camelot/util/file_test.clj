a68df67773941486cbdfc773cf2e471fdb8a4f8e
Chris Mann <chris@bitpattern.com.au>
2016-09-21 21:18:14 +1000

    Fix unit test.

--- test/clj/camelot/util/file_test.clj
+++ test/clj/camelot/util/file_test.clj
@@ -5,13 +5,15 @@
 
 (defn state
   [p]
-  {:config {:root-dir p}})
+  {:config {:root-path p}})
 
 (facts "File utils"
   (fact "Should return components relative to the root dir"
-    (sut/rel-path-components (state "/path/to") (io/file "/path/to/subdir/file")) =>
+    (sut/rel-path-components (state "/path/to")
+                             (io/file "/path/to/subdir/file")) =>
     ["subdir" "file"])
 
   (fact "Should not care if path ends in separator"
-    (sut/rel-path-components (state "/path/to/") (io/file "/path/to/subdir/file")) =>
+    (sut/rel-path-components (state "/path/to/")
+                             (io/file "/path/to/subdir/file")) =>
     ["subdir" "file"]))
