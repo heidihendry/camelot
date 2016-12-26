a3d2e6aa0665fa66c8036d12e02b0f281527a202
Chris Mann <chris@bitpattern.com.au>
2016-12-27 09:26:39 +1100

    Extend & rename snapshot script.

--- script/snapshot.sh
+++ script/snapshot.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+set -e
+
 PROJECT_NAME="camelot"
 PROJECT_FILE="project.clj"
+
+echo "Running snapshot build... "
+lein with-profiles -dev,-user,+uberjar uberjar
+
 snapshot_version="$(grep -oE "[0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT" ${PROJECT_FILE} | head -n1)"
 cp "target/${PROJECT_NAME}.jar" "target/$PROJECT_NAME-${snapshot_version}.jar"
+
+echo "Uploading snapshot... "
+scp "target/$PROJECT_NAME-${snapshot_version}.jar" "${CAMELOT_UPLOAD_TARGET}/snapshot/"
