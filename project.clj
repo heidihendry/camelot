ebae719a17d94cea8ea16ad65f9c5a37a14a4f56
Chris Mann <chris@bitpattern.com.au>
2017-03-18 17:35:08 +1100

    Version bump: 1.3.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject camelot "1.2.1-SNAPSHOT"
+(defproject camelot "1.3.0-SNAPSHOT"
   :description "Camera trap software for data management and analysis. Designed for researchers and conservationists."
   :url "http://gitlab.com/camelot-project/camelot"
   :license {:name "Eclipse Public License"
