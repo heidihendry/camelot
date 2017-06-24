1c52374abd499f850f9b3ce31cfb7271e9f58c28
Chris Mann <chris@bitpattern.com.au>
2017-06-24 17:01:38 +1000

    Update snapshot script.

--- script/prepare-snapshot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-PROJECT_NAME="camelot"
-BUILD_FILE="build.boot"
-
-snapshot_version="$(grep -oE "[0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT" ${BUILD_FILE} | head -n1)"
-cp "target/${PROJECT_NAME}.jar" "target/$PROJECT_NAME-${snapshot_version}.jar"
--- script/snapshot.sh
+++ script/snapshot.sh
@@ -3,13 +3,17 @@
 set -e
 
 PROJECT_NAME="camelot"
-PROJECT_FILE="project.clj"
+BUILD_FILE="build.boot"
 
 echo "Running snapshot build... "
 boot uberjar
 
-snapshot_version="$(grep -oE "[0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT" ${PROJECT_FILE} | head -n1)"
+snapshot_version="$(grep -oE "[0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT" ${BUILD_FILE} | head -n1)"
 cp "target/${PROJECT_NAME}.jar" "target/$PROJECT_NAME-${snapshot_version}.jar"
 
 echo "Uploading snapshot... "
 scp "target/$PROJECT_NAME-${snapshot_version}.jar" "${CAMELOT_UPLOAD_TARGET}/snapshot/"
+
+if [ -n $CAMELOT_UPLOAD_HOST ]; then
+    echo "${CAMELOT_UPLOAD_HOST}/snapshot/$PROJECT_NAME-${snapshot_version}.jar"
+fi
