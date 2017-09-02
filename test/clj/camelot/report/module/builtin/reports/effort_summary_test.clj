1c666b810ddfd71e554668087ca3792383c67eb1
Chris Mann <chris@bitpattern.com.au>
2017-09-02 18:49:10 +1000

    Allow effort summary to be ran for all surveys

--- test/clj/camelot/report/module/builtin/reports/effort_summary_test.clj
+++ test/clj/camelot/report/module/builtin/reports/effort_summary_test.clj
@@ -34,6 +34,7 @@
   {:site-id 1
    :site-name "ASite"
    :site-area 1
+   :survey-name "Survéy"
    :trap-station-id 1
    :trap-station-session-start-date (t/date-time 2015 1 1 10 10 10)
    :trap-station-session-end-date (t/date-time 2015 1 10 5 0 0)
@@ -62,7 +63,7 @@
                      (->record {}))
             state (gen-state-helper {})]
         (is (= (report state 1 records)
-               [[1 "ASite" 1 1 9 0 "2015-01-01" "2015-01-10"]]))))
+               [["Survéy" 1 "ASite" 1 1 9 0 "2015-01-01" "2015-01-10"]]))))
 
     (testing "Does not return a result should the survey not match"
       (let [records (list
@@ -75,21 +76,24 @@
                      (->record {:taxonomy-id 1})
                      (->record {:taxonomy-id 1}))
             state (gen-state-helper {})]
-        (is (= (report state 1 records) [[1 "ASite" 1 1 9 1 "2015-01-01" "2015-01-10"]]))))
+        (is (= (report state 1 records)
+               [["Survéy" 1 "ASite" 1 1 9 1 "2015-01-01" "2015-01-10"]]))))
 
     (testing "Should return 2 species, should there be 2 distinct species"
       (let [records (list
                      (->record {:taxonomy-id 1})
                      (->record {:taxonomy-id 2}))
             state (gen-state-helper {})]
-        (is (= (report state 1 records) [[1 "ASite" 1 1 9 2 "2015-01-01" "2015-01-10"]]))))
+        (is (= (report state 1 records)
+               [["Survéy" 1 "ASite" 1 1 9 2 "2015-01-01" "2015-01-10"]]))))
 
     (testing "Should return 2 trap stations, should there be 2 distinct trap stations"
       (let [records (list
                      (->record {:trap-station-id 1})
                      (->record {:trap-station-id 2}))
             state (gen-state-helper {})]
-        (is (= (report state 1 records) [[1 "ASite" 1 2 9 0 "2015-01-01" "2015-01-10"]]))))
+        (is (= (report state 1 records)
+               [["Survéy" 1 "ASite" 1 2 9 0 "2015-01-01" "2015-01-10"]]))))
 
     (testing "Should return details for each site."
       (let [records (list
@@ -99,8 +103,9 @@
                                 :site-name "Another site"
                                 :site-area 50}))
             state (gen-state-helper {})]
-        (is (= (report state 1 records) [[1 "ASite" 10 1 9 0 "2015-01-01" "2015-01-10"]
-                                         [2 "Another site" 50 1 9 0 "2015-01-01" "2015-01-10"]]))))
+        (is (= (report state 1 records)
+               [["Survéy" 1 "ASite" 10 1 9 0 "2015-01-01" "2015-01-10"]
+                ["Survéy" 2 "Another site" 50 1 9 0 "2015-01-01" "2015-01-10"]]))))
 
     (testing "Should return earliest session month for a site as the start period."
       (let [records (list
@@ -115,8 +120,9 @@
                                 :trap-station-session-id 3
                                 :trap-station-session-start-date (t/date-time 2014 7 15 1 1 1)}))
             state (gen-state-helper {})]
-        (is (= (report state 1 records) [[1 "ASite" 1 1 9 0 "2015-01-01" "2015-01-10"]
-                                         [2 "ASite" 1 1 428 0 "2014-05-15" "2015-01-10"]]))))
+        (is (= (report state 1 records)
+               [["Survéy" 1 "ASite" 1 1 9 0 "2015-01-01" "2015-01-10"]
+                ["Survéy" 2 "ASite" 1 1 428 0 "2014-05-15" "2015-01-10"]]))))
 
     (testing "Should return lastest session month for a site as the end period."
       (let [records (list
@@ -131,5 +137,6 @@
                                 :trap-station-session-id 3
                                 :trap-station-session-end-date (t/date-time 2016 2 28 1 1 1)}))
             state (gen-state-helper {})]
-        (is (= (report state 1 records) [[1 "ASite" 1 1 9 0 "2015-01-01" "2015-01-10"]
-                                         [2 "ASite" 1 1 947 0 "2015-01-01" "2016-05-30"]]))))))
+        (is (= (report state 1 records)
+               [["Survéy" 1 "ASite" 1 1 9 0 "2015-01-01" "2015-01-10"]
+                ["Survéy" 2 "ASite" 1 1 947 0 "2015-01-01" "2016-05-30"]]))))))
