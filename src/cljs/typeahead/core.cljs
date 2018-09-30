821271d11cb29b76c6b8b9a4d1374b8c7189e47e
Chris Mann <chris@bitpattern.com.au>
2018-09-30 14:32:44 +1000

    TG-485 Improve typeahead component performance

--- src/cljs/typeahead/core.cljs
+++ src/cljs/typeahead/core.cljs
@@ -242,32 +242,34 @@
        ::int-chan (chan (sliding-buffer 1))
        ::completion-chan (chan)
        ::selection-index 0})
-    om/IWillUpdate
-    (will-update [_ props state]
-      (let [si (om/get-node owner "search-input")
-            ctx (field-context (::value state)
-                               (.-selectionStart si))]
-        (when-not (= (::context state) ctx)
-          (om/set-state! owner ::context ctx)
-          (om/set-state! owner ::completions nil)
-          (let [cfn (:completion-fn (ifind data ctx))]
-            (and cfn (cfn (str/lower-case ctx) (::completion-chan state)))))
-        (let [term ((if multi-term
-                      term-at-point
-                      identity) (::value state)
-                    (.-selectionStart si))]
-          (when-not (= (::term state) term)
-            (om/set-state! owner ::term term)))
-        (go
-          (<! (timeout 100))
-          (if (= (.-activeElement js/document) si)
-            (om/set-state! owner ::is-focused true)
-            ;; Slightly delay hiding, otherwise we lose the menu as soon as we
-            ;; go to click on something.
-            (<! (timeout 100))
-            (om/set-state! owner ::is-focused false)))))
-    om/IWillMount
-    (will-mount [_]
+    om/IDidUpdate
+    (did-update [_ props state]
+      (let [value (om/get-state owner ::value)
+            si (om/get-node owner "search-input")
+            selection-start (.-selectionStart si)
+            pos (if (> selection-start (count value))
+                  (count value)
+                  selection-start)]
+        (when (not= (::cursor-position state) pos)
+          (om/set-state! owner ::cursor-position pos))
+        (let [ctx (field-context value pos)]
+          (when-not (= (::context state) ctx)
+            (om/set-state! owner ::context ctx)
+            (om/set-state! owner ::completions nil)
+            (let [cfn (:completion-fn (ifind data ctx))]
+              (and cfn (cfn (str/lower-case ctx) (::completion-chan state)))))
+          (let [term ((if multi-term
+                        term-at-point
+                        identity) value pos)]
+            (when-not (= (::term state) term)
+              (om/set-state! owner ::term term)))))
+      (go
+        (<! (timeout 300))
+        (when (and (om/get-state owner ::clear-focus)
+                   (om/get-state owner ::is-focused))
+          (om/set-state! owner ::is-focused false))))
+    om/IDidMount
+    (did-mount [_]
       (let [ic (om/get-state owner ::int-chan)
             cc (om/get-state owner ::completion-chan)]
         (go
@@ -276,10 +278,9 @@
               (if (= port ic)
                 (if (::select r)
                   (do
-                    (let [si (om/get-node owner "search-input")
-                          props (ifind data (::select r))
+                    (let [props (ifind data (::select r))
                           v (replace-term (om/get-state owner ::value)
-                                          (.-selectionStart si)
+                                          (om/get-state owner ::cursor-position)
                                           (str (::select r)
                                                (if (:field props) ":" ""))
                                           multi-term)]
@@ -287,10 +288,19 @@
                       (om/set-state! owner ::async-pending false)
                       (when (:onChange input-config)
                         ((:onChange input-config) v)))
-                    (.focus (om/get-node owner "search-input"))))
+                    (do
+                      (.focus (om/get-node owner "search-input")))))
                 (om/set-state! owner ::completions r))
               (om/set-state! owner ::selection-index 0)
-              (recur))))))
+              (recur)))))
+      (let [si (om/get-node owner "search-input")]
+        (.addEventListener si "blur" (fn [evt]
+                                       (let [t (.-target evt)]
+                                         (om/set-state! owner ::clear-focus true))))
+        (.addEventListener si "focus" (fn [evt]
+                                        (let [t (.-target evt)]
+                                          (om/set-state! owner ::is-focused true)
+                                          (om/set-state! owner ::clear-focus false))))))
     om/IRenderState
     (render-state [_ state]
       (let [v (::term state)
@@ -332,9 +342,9 @@
                                                                  (om/refresh! owner))))
                                              :onChange #(do
                                                           (let [tv (.. % -target -value)]
+                                                            (om/set-state! owner ::value tv)
                                                             (when (:onChange input-config)
                                                               ((:onChange input-config) tv))
-                                                            (om/set-state! owner ::value tv)
                                                             (when tv
                                                               (go (>! (::int-chan state) tv)))))})))
                  (when-not (and (empty? v) (empty? ctx))
