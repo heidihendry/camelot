ca8c656516ff060874c7b8920da17f04c72bb364
Chris Mann <chris@bitpattern.com.au>
2019-02-02 17:06:55 +1100

    Switch back to figwheel test-runner

--- test/cljs/camelot/test_runner.cljs
+++ test/cljs/camelot/test_runner.cljs
@@ -1,9 +1,18 @@
 (ns camelot.test-runner
-  (:require [doo.runner :refer-macros [doo-tests]]
-            [typeahead.core-test]
-            [camelot.nav-test]
-            [camelot.util.trap-station-test]))
+  (:require
+   [cljs.test :refer-macros [run-tests] :refer [report]]
+    [figwheel.main.async-result :as async-result]
+    [typeahead.core-test]
+    [camelot.nav-test]
+    [camelot.util.trap-station-test]))
 
-(doo-tests 'typeahead.core-test
-           'camelot.nav-test
-           'camelot.util.trap-station-test)
+;; tests can be asynchronous, we must hook test end
+(defmethod report [:cljs.test/default :end-run-tests] [test-data]
+  (if (cljs.test/successful? test-data)
+    (async-result/send "Tests passed!")
+    (async-result/throw-ex (ex-info "Tests Failed" test-data))))
+
+(defn -main [& args]
+  (run-tests 'typeahead.core-test 'camelot.nav-test 'camelot.util.trap-station-test)
+  ;; return a message to the figwheel process that tells it to wait
+  [:figwheel.main.async-result/wait 5000])
