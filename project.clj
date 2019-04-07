73558ab3d7efd68f8670529bcf6b21d335f01cd8
Chris Mann <chris@bitpattern.com.au>
2019-04-07 14:41:19 +1000

    Bump snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject camelot "1.5.1"
+(defproject camelot "1.5.2-SNAPSHOT"
   :description "Manage and analyse camera trap data. Designed for researchers and conservationists."
   :url "http://gitlab.com/camelot-project/camelot"
   :license {:name "AGPL v3"
