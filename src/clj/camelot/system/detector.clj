3172ce2e31ab482a5f8febd20bd4b94948a3e68d
Chris Mann <chris@bitpattern.com.au>
2020-01-01 19:43:44 +1030

    Write state out periodically to reduce cpu load

--- src/clj/camelot/system/detector.clj
+++ src/clj/camelot/system/detector.clj
@@ -16,6 +16,11 @@
   "Name of the detector state file."
   "detector.edn")
 
+(def state-serialisation-backoff
+  "Delay in ms between attempts to write out the current detector state.
+  Idempotence is order-of-the-day; we afford a few seconds of loss."
+  (* 15 1000))
+
 (defn- detector-path
   [state]
   (let [file (io/file (-> state :config :path :database) detector-filename)]
@@ -45,16 +50,19 @@
                                     :action "startup"
                                     :ni true})
             (log/info "Starting detector...")
-            (let [detector-state (duratom/duratom :local-file
+            (let [latest-state (async/chan (async/sliding-buffer 1))
+                  detector-state (duratom/duratom :local-file
                                                   :file-path (detector-path state)
                                                   :init {}
                                                   :rw {:read nippy/thaw-from-file
-                                                       :write nippy/freeze-to-file})
+                                                       :write (fn [file data]
+                                                                (async/go (async/>! latest-state [file data])))})
                   cmd-chan (async/chan)
-                  cmd-mult (async/mult cmd-chan)
-                  event-chan (detection/run state detector-state cmd-mult)]
+                  cmd-mult (async/mult cmd-chan)]
+
               (let [kf (juxt (constantly :events) :subject :action)
-                    int-cmd-ch (async/chan)]
+                    int-cmd-ch (async/chan)
+                    event-chan (detection/run state detector-state cmd-mult)]
                 (async/tap cmd-mult int-cmd-ch)
                 (async/go-loop []
                   (let [[v port] (async/alts! [int-cmd-ch event-chan])]
@@ -71,6 +79,34 @@
                           (swap! detector-state update-in (kf v) (fnil inc 0)))
                         (analytics/track state (event-to-analytics v))
                         (recur))))))
+
+              (let [int-cmd-ch (async/chan)]
+                (async/tap cmd-mult int-cmd-ch)
+                (async/go-loop []
+                  (let [timeout-ch (async/timeout state-serialisation-backoff)
+                        [v port] (async/alts! [int-cmd-ch timeout-ch])]
+                    (condp = port
+                      int-cmd-ch
+                      (if (= (:cmd v) :stop)
+                        (do
+                          (if-let [[file data] (async/poll! latest-state)]
+                            (do
+                              (nippy/freeze-to-file file data)
+                              (log/info "Updated state file due to shutdown")))
+                          (swap! detector-state assoc-in [:system-status] :offline))
+                        (recur))
+
+                      timeout-ch
+                      (let [_ (async/<! timeout-ch)]
+                        (try
+                          (if-let [[file data] (async/poll! latest-state)]
+                            (do
+                              (nippy/freeze-to-file file data)
+                              (log/info "Updated state file")))
+                          (catch Exception e
+                            (log/error "Exception while writing state file" e)))
+                        (recur))))))
+
               (assoc this
                      :state detector-state
                      :cmd-chan cmd-chan))))
