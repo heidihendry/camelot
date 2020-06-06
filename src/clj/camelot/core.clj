ea22e59eb03cb874cc0d9dea43763c8504535c22
Chris Mann <chris@bitpattern.com.au>
2020-06-06 23:14:10 +1000

    Merge camelot.system.core into camelot.core

--- src/clj/camelot/core.clj
+++ src/clj/camelot/core.clj
@@ -1,13 +1,51 @@
 (ns camelot.core
   "Camelot - Camera Trap management software for conservation research."
   (:require
-   [com.stuartsierra.component :as component]
-   [camelot.system.core :as system])
+   [camelot.system.config.core :as config]
+   [camelot.system.http.core :as http]
+   [camelot.system.db.core :as db]
+   [camelot.system.migrater :as migrater]
+   [camelot.system.backup-manager :as backup-manager]
+   [camelot.system.datasets :as datasets]
+   [camelot.system.importer :as importer]
+   [camelot.system.detector :as detector]
+   [com.stuartsierra.component :as component])
   (:gen-class))
+
+(defn- camelot-system
+  [system-overrides]
+  (let [smap
+        (apply component/system-map
+               (apply concat
+                      (merge {:config (config/map->Config {})
+                              :database (db/map->Database {})
+                              :backup-manager (backup-manager/map->BackupManager {})
+                              :datasets (datasets/map->Datasets {})
+                              :migrater (migrater/map->Migrater {})
+                              :importer (importer/map->Importer {})
+                              :detector (detector/map->Detector {})
+                              :app (http/->HttpServer {})}
+                             system-overrides)))]
+    (component/system-using smap {:app {:config :config
+                                        :database :database
+                                        :importer :importer
+                                        :detector :detector
+                                        :datasets :datasets
+                                        :backup-manager :backup-manager}
+                                  :config {}
+                                  :database {}
+                                  :migrater {:backup-manager :backup-manager}
+                                  :datasets {:config :config
+                                             :database :database
+                                             :migrater :migrater}
+                                  :importer {:config :config}
+                                  :detector {:config :config
+                                             :database :database
+                                             :datasets :datasets}})))
 
 (defn start-prod
   []
-  (component/start (system/camelot-system {})))
+  (component/start (camelot-system {})))
 
 (defn -main [& _]
   (start-prod))
